cb47cea6bb7e3f8e102137220918fe1ff10fd325
Timothy Merritt <mobile.vulgus@protonmail.com>
2020-05-18 12:04:39 -0700

    Updated README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Hank Math!
 
+![Start screen](screenshots/hank_math1.png)
+
 This is a simple Python program I wrote for my son to practice math problems (and for me to practice Python). It randomly selects two integers between a defined range, gets the user's input to the problem, and records all the variables of the problem(s) in class instances to show a final score. If you want to play it, it should run fine in most terminals.
--- hank_math.py
+++ hank_math.py
@@ -1,23 +1,13 @@
-# HANK MATH! --version 2.0
-# Copyright Timothy Merritt 2020
+# HANK MATH!
+# by Timothy Merritt 2020
 #
 # A simple program for practicing math problems. Displays addition
 # and subtraction problems, tracks results, and exports scores to 
 # separate file for score-keeping.
 #
-# This file is part of Hank Math. 
-#
-# Hank Math is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License as published by 
-# the Free Software Foundation, either version 3 of the License, or 
-# (at your option) any later version.
-# Foobar is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public 
-# License along with Foobar. If not, see http://www.gnu.org/licenses/.
+# To do:
+# * Use class instances to generate tabulate-style table with final
+# results
 # --------------------------------------------------------------------
 
 import random, os, datetime, platform
